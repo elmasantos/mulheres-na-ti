05006bef9e6d97762ea8fc6e46cba1eda754f69f
Elma Santos <elmasantos94@hotmail.com>
2017-11-02 17:26:58 -0300

    Adicionando arquivo de dataset e script para ler os dados.

---
+++ read_dataset.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python                                                                        
+# -*- coding: utf-8 -*-
+
+import pandas as pd 
+
+colnames =["discente", "sexo", "nota_final", "cotista", "cep", "descricao"]
+discentes_dataset = pd.read_csv('data/discente.csv', names = colnames)
+
+id_aluno = discentes_dataset.discente.tolist()
+sexo = discentes_dataset.sexo.tolist()
+nota_final = discentes_dataset.nota_final.tolist()
+cotista = discentes_dataset.cotista.tolist()
+cep = discentes_dataset.cep.tolist()
+descricao = discentes_dataset.descricao.tolist()
+
