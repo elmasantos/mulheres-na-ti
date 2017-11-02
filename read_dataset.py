5be1bc2d4ad79785c3afe2bc7f469b5fc4c99a8f
Elma Santos <elmasantos94@hotmail.com>
2017-11-02 19:30:02 -0300

    Removendo dados faltantes do dataset.

--- read_dataset.py
+++ read_dataset.py
@@ -6,11 +6,17 @@ import pandas as pd
 colnames =["discente", "sexo", "nota_final", "cotista", "cep", "descricao"]
 discentes_dataset = pd.read_csv('data/discente.csv', names = colnames)
 
-id_aluno = discentes_dataset.discente.tolist()
-sexo = discentes_dataset.sexo.tolist()
-nota_final = discentes_dataset.nota_final.tolist()
-cotista = discentes_dataset.cotista.tolist()
-cep = discentes_dataset.cep.tolist()
-descricao = discentes_dataset.descricao.tolist()
+#novo dataframe sem missing data
+dataset_no_missing = discentes_dataset.dropna()
+#criando novo arquivo csv sem missing data
+dataset_no_missing.to_csv("data/discente_no_missing.csv")
+
+id_aluno = dataset_no_missing.discente.tolist()
+sexo = dataset_no_missing.sexo.tolist()
+nota_final = dataset_no_missing.nota_final.tolist()
+cotista = dataset_no_missing.cotista.tolist()
+cep = dataset_no_missing.cep.tolist()
+descricao = dataset_no_missing.descricao.tolist()
 
 tamanho = len(id_aluno)
+print(tamanho)
