aa429638b2d2b0e637cd5bb0b4307dff58ba488b
Elma Santos <elmasantos94@hotmail.com>
2017-11-19 01:07:54 -0300

    Tratando datasets.

--- data_processing.py
+++ data_processing.py
@@ -3,12 +3,6 @@
 
 import pandas as pd 
 
-#id do curso tecnologia da informação
-id_curso_ti = 92127264
-#leitura de arquivo
-discentes_df = pd.read_csv('data/discentes2.csv', sep=';')
-matriculas20172_df = pd.read_csv('data/matricula-componente-20172.csv', sep=';')
-
 
 #seleção de dados do dataset
 def select_data(dataframe, column_notnull, column_selected, name):
@@ -27,6 +21,30 @@ def fill_nandata(dataframe, column, data):
 
 	return dataframe
 
+#limpando dataframe
+def matricula_drop_data(dataframe, column):
+	#excluindo coluna desnecessária	
+	dataframe.drop(column, axis=1, inplace=True)
+	#excluindo registros com descrição 'EXCLUIDA' ou 'CANCELADO'
+	dataframe = dataframe[dataframe.descricao != 'EXCLUIDA']
+	dataframe = dataframe[dataframe.descricao != 'CANCELADO']
+	dataframe = dataframe[dataframe.descricao != 'INDEFERIDO']
+	
+	return dataframe
+
+
+#id do curso tecnologia da informação
+id_curso_ti = 92127264
+
+#leitura de arquivos
+discentes_df = pd.read_csv('data/discentes.csv', sep=';')
+matriculas20141_df = pd.read_csv('data/matricula-componente-20141.csv', sep=';')
+matriculas20142_df = pd.read_csv('data/matricula-componente-20142.csv', sep=';')
+matriculas20151_df = pd.read_csv('data/matriculas-de-2015.1.csv', sep=';')
+matriculas20152_df = pd.read_csv('data/matriculas-de-2015.2.csv', sep=';')
+matriculas20161_df = pd.read_csv('data/matriculas-de-2016.1.csv', sep=';')
+matriculas20162_df = pd.read_csv('data/matriculas-de-2016.2.csv', sep=';')
+
 #discentes dataset
 df_no_missing = select_data(discentes_df, 'id_discente', 'nivel_ensino', 'GRADUAÇÃO')
 df_no_missing = fill_nandata(df_no_missing, 'cep', '59000-000')
@@ -40,45 +58,39 @@ df_no_missing['cep'] = df_no_missing['cep'].str.replace('-', '')
 df_no_missing['cep'] = df_no_missing['cep'].str.replace('.', '')
 df_no_missing['cep'] = df_no_missing['cep'].str.replace(' ', '')
 
+
 #matriculas dataset
-print(len(matriculas20172_df))
-
-mat_no_missing = select_data(matriculas20172_df, 'discente', 'id_curso', id_curso_ti)
-#excluindo coluna desnecessária
-mat_no_missing.drop('faltas_unidade', axis=1, inplace=True)
-print(mat_no_missing.head(10))
-
-
+mat20141_no_missing = select_data(matriculas20141_df, 'discente', 'id_curso', id_curso_ti)
+mat20142_no_missing = select_data(matriculas20142_df, 'discente', 'id_curso', id_curso_ti)
+mat20151_no_missing = select_data(matriculas20151_df, 'discente', 'id_curso', id_curso_ti)
+mat20152_no_missing = select_data(matriculas20152_df, 'discente', 'id_curso', id_curso_ti)
+mat20161_no_missing = select_data(matriculas20161_df, 'discente', 'id_curso', id_curso_ti)
+mat20162_no_missing = select_data(matriculas20162_df, 'discente', 'id_curso', id_curso_ti)
 
 
-#novo arquivo csv sem missing values, sem outliers e apenas graduação
-#df_no_missing.to_csv("data/discentes_processed2.csv")
+mat20141_no_missing = matricula_drop_data(mat20141_no_missing, 'faltas_unidade')
+mat20142_no_missing = matricula_drop_data(mat20142_no_missing, 'faltas_unidade')
+mat20151_no_missing = matricula_drop_data(mat20151_no_missing, ['faltas_unidade', 'Unnamed: 10'])
+mat20152_no_missing = matricula_drop_data(mat20152_no_missing, ['faltas_unidade', 'Unnamed: 10'])
+mat20161_no_missing = matricula_drop_data(mat20161_no_missing, ['faltas_unidade', 'Unnamed: 10'])
+mat20162_no_missing = matricula_drop_data(mat20162_no_missing, ['faltas_unidade', 'Unnamed: 10'])
 
 
-'''
-#preenchimento de campos de CEP vazios
-value = {'cep': '59000-000', 'forma_ingresso' : 'forma de ingresso'}
-df_no_missing.fillna(value=value, inplace=True)
+#convertendo dados em string
+mat20141_no_missing = mat20141_no_missing.astype(str)
+mat20142_no_missing = mat20142_no_missing.astype(str)
+mat20151_no_missing = mat20151_no_missing.astype(str)
+mat20152_no_missing = mat20152_no_missing.astype(str)
+mat20161_no_missing = mat20161_no_missing.astype(str)
+mat20162_no_missing = mat20162_no_missing.astype(str)
 
 
-#preenchimento de campos de CEP com valor 59
-df_no_missing.replace('59', '59000-000', inplace=True)
 
-#retirando máscara de CEP, pontos e espaços
-df_no_missing['cep'] = df_no_missing['cep'].str.replace('-', '')
-df_no_missing['cep'] = df_no_missing['cep'].str.replace('.', '')
-df_no_missing['cep'] = df_no_missing['cep'].str.replace(' ', '')
 
-#novo arquivo csv sem missing values, sem outliers e apenas graduação
-<<<<<<< HEAD
-#df_no_missing.to_csv("data/discentes_processed2.csv")'''
-'''print(matriculas20172_df.dtypes)
-
-df_matriculas_no_missing = matriculas20172_df[pd.notnull(matriculas20172_df['discente'])]
-print(len(df_matriculas_no_missing))
-
-df_matriculas_no_missing = df_matriculas_no_missing[df_matriculas_no_missing.id_curso == id_curso_ti]
-
-print(len(df_matriculas_no_missing))'''
-
-df_no_missing.to_csv("data/discentes_processed.csv")
+df_no_missing.to_csv("data/processed/discentes_processed.csv")
+mat20141_no_missing.to_csv("data/processed/matriculas_20141_processed.csv")
+mat20142_no_missing.to_csv("data/processed/matriculas_20142_processed.csv")
+mat20151_no_missing.to_csv("data/processed/matriculas_20151_processed.csv")
+mat20152_no_missing.to_csv("data/processed/matriculas_20152_processed.csv")
+mat20161_no_missing.to_csv("data/processed/matriculas_20161_processed.csv")
+mat20162_no_missing.to_csv("data/processed/matriculas_20162_processed.csv")
