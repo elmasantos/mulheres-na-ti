f14bd3be63a513cab825b4a55e6e88957f21ccbb
Elma Santos <elmasantos94@hotmail.com>
2017-11-26 14:05:06 -0300

    Ajusta para tratar csv de matricula ou discentes

--- data_processing.py
+++ data_processing.py
@@ -6,8 +6,8 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser(description='Script para tratar arquivos csv')
-parser.add_argument('-f','--file', help='Arquivo CSV de matrículas', required=True)
-parser.add_argument('-s','--semestre', help='Semestre das matrículas', required=True)
+parser.add_argument('-f','--file', help='Arquivo CSV de matrículas', required=False)
+parser.add_argument('-s','--semestre', help='Semestre das matrículas', required=False)
 
 args = parser.parse_args()
 
@@ -38,48 +38,50 @@ def mat_fill_and_drop_data(dataframe, column):
 	
 	return dataframe
 
-
-#guardando nome do arquivo e semestre
-matriculas_file = args.file
-semestre_matriculas = args.semestre
-
-#leitura de arquivos
-discentes_df = pd.read_csv('data/discentes.csv', sep=';')
-matriculas_df = pd.read_csv(matriculas_file, sep=';')
-
 #id do curso tecnologia da informação
 id_curso_ti = 92127264
 
-#discentes dataset
-df_no_missing = select_data(discentes_df, 'id_discente', 'nivel_ensino', 'GRADUAÇÃO')
-#preenchimento de campos vazios
-values = {'cep' : '59000000', 'forma_ingresso' : 'forma de ingresso'}
-df_no_missing.fillna(value=values, inplace=True)
-#preenchimento de campos de CEP com valor 59
-df_no_missing.replace('59', '59000-000', inplace=True)
-#retirando máscara de CEP, pontos e espaços
-df_no_missing['cep'] = df_no_missing['cep'].str.replace('-', '')
-df_no_missing['cep'] = df_no_missing['cep'].str.replace('.', '')
-df_no_missing['cep'] = df_no_missing['cep'].str.replace(' ', '')
+if args.file is not None:
+	#guardando nome do arquivo e semestre
+	matriculas_file = args.file
+	semestre_matriculas = args.semestre
 
+	#leitura de arquivo
+	matriculas_df = pd.read_csv(matriculas_file, sep=';')
+	#matriculas dataset
+	matriculas_no_missing = select_data(matriculas_df, 'discente', 'id_curso', id_curso_ti)
+	#limpeza
+	if args.semestre == '20141' or args.semestre == '20142':
+		matriculas_no_missing = mat_fill_and_drop_data(matriculas_no_missing, ['faltas_unidade'])
+	else:
+		matriculas_no_missing = mat_fill_and_drop_data(matriculas_no_missing, ['faltas_unidade', 'Unnamed: 10'])
 
-#matriculas dataset
-matriculas_no_missing = select_data(matriculas_df, 'discente', 'id_curso', id_curso_ti)
-#limpeza
-if args.semestre == '20141' or args.semestre == '20142':
-	matriculas_no_missing = mat_fill_and_drop_data(matriculas_no_missing, ['faltas_unidade'])
+	#convertendo notas para float
+	matriculas_no_missing['nota'] = matriculas_no_missing['nota'].astype(float)
+
+	#alterando notas para 2 casas decimais
+	matriculas_no_missing = matriculas_no_missing.round({'nota' : 2})
+
+	#convertendo dados para string
+	matriculas_no_missing = matriculas_no_missing.astype(str)
+
+	matriculas_no_missing.to_csv("data/processed/matriculas_"+args.semestre+"_processed.csv")
+
 else:
-	matriculas_no_missing = mat_fill_and_drop_data(matriculas_no_missing, ['faltas_unidade', 'Unnamed: 10'])
+	#leitura de arquivos
+	discentes_df = pd.read_csv('data/dados-pessoais-discentes.csv', sep=';')
 
-#convertendo notas para float
-matriculas_no_missing['nota'] = matriculas_no_missing['nota'].astype(float)
+	#discentes dataset
+	df_no_missing = select_data(discentes_df, 'id_discente', 'nivel_ensino', 'GRADUAÇÃO')
+	#preenchimento de campos vazios
+	values = {'forma_ingresso' : 'forma de ingresso'}
+	df_no_missing.fillna(value=values, inplace=True)
+	#preenchimento de campos de CEP com valor 59
+	'''df_no_missing.replace('59', '59000-000', inplace=True)
+	#retirando máscara de CEP, pontos e espaços
+	df_no_missing['cep'] = df_no_missing['cep'].str.replace('-', '')
+	df_no_missing['cep'] = df_no_missing['cep'].str.replace('.', '')
+	df_no_missing['cep'] = df_no_missing['cep'].str.replace(' ', '')'''
 
-#alterando notas para 2 casas decimais
-matriculas_no_missing = matriculas_no_missing.round({'nota' : 2})
-
-#convertendo dados para string
-matriculas_no_missing = matriculas_no_missing.astype(str)
-
-
-df_no_missing.to_csv("data/processed/discentes_processed.csv")
-matriculas_no_missing.to_csv("data/processed/matriculas_"+args.semestre+"_processed.csv")
+	
+	df_no_missing.to_csv("data/processed/dados_pessoais_discentes_processed.csv")
