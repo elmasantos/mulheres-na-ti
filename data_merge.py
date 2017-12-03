f89fecce790e38ca32ece6bd496644b83443e455
Elma Santos <elmasantos94@hotmail.com>
2017-12-03 09:44:06 -0300

    Altera merge

--- data_merge.py
+++ data_merge.py
@@ -2,23 +2,29 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser(description='Script para cruzar tabelas')
-parser.add_argument('-f','--file', help='Tabela de matrículas processada', required=True)
-parser.add_argument('-s','--semestre', help='Semestre das matrículas', required=False)
+parser.add_argument('-m','--mat', help='Tabela de matrículas processada', required=True)
+parser.add_argument('-se','--socioeco', help='Tabela de socio-econômicos processada', required=True)
+parser.add_argument('-s','--semestre', help='Semestre referência', required=False)
 
 args = parser.parse_args()
 
 semestre_matriculas = args.semestre
-matriculas_file = args.file
+matriculas_file = args.mat
+socioeconomicos_file = args.socioeco
 
 discentes_df = pd.read_csv('data/processed/dados_discentes_processed.csv', sep=',')
 matriculas_df = pd.read_csv(matriculas_file, sep=',')
+socioeco_df = pd.read_csv(socioeconomicos_file, sep=';')
 
 print(discentes_df.columns.tolist)
 print(matriculas_df.columns.tolist)
-merge = pd.merge(discentes_df, matriculas_df, how='inner', left_on='id_discente', right_on='discente')
+print(socioeco_df.columns.tolist)
+
+merge = pd.merge(pd.merge(discentes_df, matriculas_df, how='inner', left_on='id_discente', right_on='discente'),
+	socioeco_df, how='inner', on='id_discente')
 
 #deletando colunas 'Unnamed: 0_x' e 'Unnamed: 0_y'
-merge.drop(['Unnamed: 0_x', 'Unnamed: 0_y'], axis=1, inplace=True)
+merge.drop(['Unnamed: 0_x', 'Unnamed: 0_y', 'discente'], axis=1, inplace=True)
 
 #preenchendo campos vazios
 values = {'estado_origem' : 'NAO INFORMADO', 'cidade_origem' : 'NAO INFORMADO',
