fa476ef03f1a74c99f32ca48a0668e149ddd4bcd
Elma Santos <elmasantos94@hotmail.com>
2017-12-05 15:35:36 -0300

    Gera graficos

--- results.py
+++ results.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import pandas as pd 
 import argparse
 
@@ -11,10 +11,27 @@ args = parser.parse_args()
 #gera CSV de quantidade de alunos por semestre
 def generate_amount_csv(itens):
 	df = pd.DataFrame(itens, columns=['Semestre', 'Quantidade de alunos', 'Homens', 'Mulheres'])
-	df.to_csv("data/resultados/quantidade_pessoas_ativas.csv", sep=';', index=False)
+	plt.figure()
+	df.plot.bar(x='Semestre')
+	plt.show()
+	#df.to_csv("data/resultados/quantidade_pessoas_ativas.csv", sep=';', index=False)
 
-def statistic():
-	pass
+def generate_bairro_mulheres(dataframe, semestre):
+	dataframe = dataframe[dataframe.sexo == 'F']
+	grouped = dataframe.groupby(['bairro'])['sexo'].count()
+	#plt.figure()
+	#grouped.plot.bar(title=semestre +" - Quantidade de mulheres x bairros")
+	#plt.show()
+	df = pd.DataFrame(grouped, columns=['sexo', 'bairro'])
+	#grouped.to_csv("data/resultados/mulheres_bairro"+semestre+".csv", sep=';', index=False)
+
+def generate_cidade_mulheres(dataframe, semestre):
+	dataframe = dataframe[dataframe.sexo == 'F']
+	grouped = dataframe.groupby(['cidade_origem'])['sexo'].count()
+	#plt.figure()
+	#grouped.plot.bar(title=semestre +" - Quantidade de mulheres x cidade de origem")
+#	plt.show()
+
 
 semestres = args.semestre
 semestres = semestres.split(',')
@@ -24,12 +41,13 @@ periodo = []
 quantidade = []
 homens = []
 mulheres = []
-bairro = []
+bairros = []
 
 #lendo dataframes
 for sem in semestres:
 	dataframe = pd.read_csv("data/merge/merge"+sem+"_sem_rep.csv", sep=';')
 	dataframes.append(dataframe)
+
 
 #coletando dados
 for i, dataframe in enumerate(dataframes):
@@ -38,6 +56,9 @@ for i, dataframe in enumerate(dataframes):
 	homens.append(len(dataframe[dataframe.sexo == 'M']))
 	mulheres.append(len(dataframe[dataframe.sexo == 'F']))
 
+	generate_bairro_mulheres(dataframe, semestres[i])
+	generate_cidade_mulheres(dataframe, semestres[i])
+
+
 raw_data = {'Semestre' : periodo, 'Quantidade de alunos' : quantidade, 'Homens' : homens, 'Mulheres' : mulheres}
 generate_amount_csv(raw_data)
-
